df1e5e73de163dc34f859ce29689f70bb18af0e5
sakhawat9 <sakhawathossain7969@gmail.com>
2022-01-29 00:08:16 +0600

    first commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Pizza Restaurant in Bangladesh</title>
+        <meta name="description" content="Bast pizza shop in Dhaka" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      Home pages
+    </div>
+  );
+}
