4a9589b4f452628a03cacf001f823fc49e00dfda
sakhawat9 <sakhawathossain7969@gmail.com>
2022-01-30 19:23:36 +0600

    [ADD] cart add

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,9 @@
-
 import Image from "next/image";
+import { useSelector } from "react-redux";
 import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
+  const quantity = useSelector((state) => state.cart.quantity);
   return (
     <div className={styles.container}>
       <div className={styles.item}>
@@ -28,7 +29,7 @@ const Navbar = () => {
       <div className={styles.item}>
         <div className={styles.cart}>
           <Image src="/img/cart.png" alt="" width="30px" height="30px" />
-          <div className={styles.counter}>2</div>
+          <div className={styles.counter}>{quantity}</div>
         </div>
       </div>
     </div>
