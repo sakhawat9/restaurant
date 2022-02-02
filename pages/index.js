baa98e6428145a774a685845596058455d16cea3
sakhawat9 <sakhawathossain7969@gmail.com>
2022-02-02 12:25:35 +0600

    test

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 import Head from "next/head";
 import { useState } from "react";
-import Add from "../components/Add";
-import AddButton from "../components/AddButton";
-import Featured from "../components/Featured";
-import PizzaList from "../components/PizzaList";
 import styles from "../styles/Home.module.css";
 
 export default function Home({ pizzaList, admin }) {
@@ -17,11 +13,11 @@ export default function Home({ pizzaList, admin }) {
         <meta name="description" content="Bast pizza shop in Dhaka" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Featured />
+      {/* <Featured />
       {admin && <AddButton setClose={setClose} />}
-      {!close && <Add setClose={setClose} />}
+      {!close && <Add setClose={setClose} />} */}
       <h2>Hello World!</h2>
-      <PizzaList pizzaList={pizzaList} />
+      {/* <PizzaList pizzaList={pizzaList} /> */}
     </div>
   );
 }
