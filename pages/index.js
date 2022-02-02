fc0c159954afa3eb51bcf07b7f7d36961b575bd9
sakhawat9 <sakhawathossain7969@gmail.com>
2022-02-02 12:30:08 +0600

    test

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
+import axios from "axios";
 import Head from "next/head";
 import { useState } from "react";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ pizzaList, admin }) {
 
   const [close, setClose] = useState(true);
   return (
@@ -21,19 +22,19 @@ export default function Home() {
   );
 }
 
-// export const getServerSideProps = async (ctx) => {
-//   const myCookie = ctx.req?.cookies || "";
-//   let admin = false;
+export const getServerSideProps = async (ctx) => {
+  const myCookie = ctx.req?.cookies || "";
+  let admin = false;
 
-//   if (myCookie.token === process.env.TOKEN) {
-//     admin = true;
-//   }
+  if (myCookie.token === process.env.TOKEN) {
+    admin = true;
+  }
 
-//   const res = await axios.get("http://localhost:3000/api/products");
-//   return {
-//     props: {
-//       pizzaList: res.data,
-//       admin,
-//     },
-//   };
-// };
+  const res = await axios.get("http://localhost:3000/api/products");
+  return {
+    props: {
+      pizzaList: res.data,
+      admin,
+    },
+  };
+};
