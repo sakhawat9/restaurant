02d6755e47f3cea2a7322380d18659e7b13f0f08
sakhawat9 <sakhawathossain7969@gmail.com>
2022-02-02 12:42:13 +0600

    test

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 import Head from "next/head";
 import { useState } from "react";
+import Add from "../components/Add";
+import AddButton from "../components/AddButton";
+import Featured from "../components/Featured";
+import PizzaList from "../components/PizzaList";
 import styles from "../styles/Home.module.css";
 
 export default function Home({ pizzaList, admin }) {
-
+  console.log(pizzaList);
   const [close, setClose] = useState(true);
   return (
     <div className={styles.container}>
@@ -13,11 +17,10 @@ export default function Home({ pizzaList, admin }) {
         <meta name="description" content="Bast pizza shop in Dhaka" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <Featured />
+      <Featured />
       {admin && <AddButton setClose={setClose} />}
-      {!close && <Add setClose={setClose} />} */}
-      <h2>Hello World!</h2>
-      {/* <PizzaList pizzaList={pizzaList} /> */}
+      {!close && <Add setClose={setClose} />}
+      <PizzaList pizzaList={pizzaList} />
     </div>
   );
 }
@@ -30,7 +33,7 @@ export const getServerSideProps = async (ctx) => {
     admin = true;
   }
 
-  const res = await axios.get("http://localhost:3000/api/products");
+  const res = await axios.get(`${process.env.API_URL}/api/products`);
   return {
     props: {
       pizzaList: res.data,
